249ef972045c4860657a0a26d08099befff59d7a
Bipin Paul Bedi <bipinpaulbedi@MacBook-Pro.local>
2020-07-20 21:59:53 +1000

    updated version

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Novela by Narative`,
     name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
+    siteUrl: `http://v2.bipinpaulbedi.com`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
       heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
